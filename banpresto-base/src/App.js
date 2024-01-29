b7cc416d0a201fe26e202361b72bddf0333ce4be
Venturing Guy <LVentura335@Gmail.com>
2024-01-29 01:07:55 -0800

    Routes added

--- banpresto-base/src/App.js
+++ banpresto-base/src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Main from './components/Main/Main.tsx';
+import YearPage from './components/Main/YearPage/YearPage.tsx';
 
 function App() {
   return (
-    <Main/>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Main/>}/>
+        <Route path="/banpresto-games-by-year/:year" element={<YearPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
