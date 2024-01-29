4dd33922fcd747dbb550674e792b994f14776c9b
Venturing Guy <LVentura335@Gmail.com>
2024-01-28 19:05:51 -0800

    main styled components

---
+++ banpresto-base/src/components/Main/Main.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  width           : 100vw;
+  height          : 100vh;
+  background-color: black;
+  color           : white;
+  display         : flex;
+  align-items     : center;
+  flex-direction  : column;
+`;
+
+export const MainImage = styled.img`
+  width  : 370px;
+  height : 324px;
+  padding: 24px 16px;
+`;
