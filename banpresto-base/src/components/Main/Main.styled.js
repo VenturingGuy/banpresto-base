217c068385a00e7e5aa35a501eefa05cd5ffb75b
Venturing Guy <LVentura335@Gmail.com>
2024-01-28 19:29:13 -0800

    style updates

--- banpresto-base/src/components/Main/Main.styled.js
+++ banpresto-base/src/components/Main/Main.styled.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-  width           : 100vw;
-  height          : 100vh;
+  max-width           : 100%;
+  height          : 100%;
   background-color: black;
   color           : white;
   display         : flex;
@@ -14,4 +14,26 @@ export const MainImage = styled.img`
   width  : 370px;
   height : 324px;
   padding: 24px 16px;
+`;
+
+export const MainYearSection = styled.div`
+  width : 80%;
+  height: 20%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const MainYearCTA = styled.div`
+
+`;
+
+export const MainYearList = styled.ul`
+  display: flex;
+  max-width: 100%;
+  height: 100px;
+`;
+
+export const MainYearListItem = styled.a`
+padding: 4px;
 `;
