1d1c1141fe4a05639665b4e796e0a52dc9481428
Venturing Guy <LVentura335@Gmail.com>
2024-01-28 19:29:19 -0800

    component updates

--- banpresto-base/src/components/Main/Main.tsx
+++ banpresto-base/src/components/Main/Main.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import home from '../../assets/home.jpg';
-import { MainContainer, MainImage } from './Main.styled.js';
+import { MainContainer, MainImage, MainYearCTA, MainYearList, MainYearListItem, MainYearSection } from './Main.styled.js';
+
+const CURRENT_YEAR = new Date().getFullYear();
+const BANPRESTO_FIRST_GAME_YEAR = 1990;
+
+const renderYears = () => {
+  const years = [];
+  for (let i = BANPRESTO_FIRST_GAME_YEAR; i <= CURRENT_YEAR; i++){
+    console.log(i);
+    years.push(i);
+  }
+  return (
+    <MainYearList>
+      {years.map(year => (
+        <MainYearListItem>{year}</MainYearListItem>
+      ))}
+    </MainYearList>
+  )
+}
+
+console.log(CURRENT_YEAR);
 
 const Main: () => JSX.Element = () => {
   return(
     <MainContainer>
       <MainImage alt="home" src={home}/>
       <h1>Antique Game Research Society</h1>
+      <MainYearSection>
+        Banpresto By Year
+        <MainYearCTA>
+          {renderYears()}
+        </MainYearCTA>
+      </MainYearSection>
     </MainContainer>
   );
 }
